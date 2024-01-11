224af2ed29a93afba75239a94e4fd0917fee68a7
Jeremie-mengi <jeremiemengi248@gmail.com>
2024-01-11 18:36:40 +0100

    initial-sprint3-Style

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -6,19 +6,18 @@ import TweetLikes from "./TweetLikes";
 function Tweet(){
     const tweetEntier = twitter.map((item) => (
    
-         <div key= {item.id} className="tweet">
- <a href="#" className="tweet-avatar img" ><img src={item.avatar} alt=""/></a> 
-
-      <div className="tweet-body">
-      <p className="tweet-title" >
-   <span className="tweet-title-author">{item.title}</span>
+         <div key= {item.id} className="flex justify-start items-start gap-0.5  pt-6 pr-4 border-b border-b-gray-800"> 
+ <a href="#" className="max-w-full max-h-full rounded-full"  >< img  src={item.avatar} alt=""/></a> 
+      <div className="flex flex-col gap-2">
+      <p className="flex justify-start items-start gap-0.5 text-base" >
+   <span className="text-base">{item.title}</span>
    <span><a href="#"><img src={Ver}/></a></span>
-   <span className="tweet-title-details">{item.titleDetails}</span>
-   <span className="tweet-title-details">.</span>
-   <span className="tweet-title-details">{item.timeTtweetDetails}</span>
+   <span className="text-gray-400">{item.titleDetails}</span>
+   <span className="text-gray-400">.</span>
+   <span className="text-gray-400">{item.timeTtweetDetails}</span>
    </p>
 
-    <p className="tweet-text">{item.text}</p>
+    <p className="text-base text-slate-200">{item.text}</p>
 
     <div className= "tweet-image"> <img src={item.image} alt=""/>
      </div>
